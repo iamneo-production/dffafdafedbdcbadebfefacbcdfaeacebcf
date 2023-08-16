148e05a836759765fdf3223e933ab60963ca2f60
Iamneo <techadmin@iamneo.ai>
2023-08-16 02:02:30 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/configurations/MyUserDetailsService.java
+++ springapp/src/main/java/com/examly/springapp/configurations/MyUserDetailsService.java
@@ -1,5 +1,27 @@
 package com.examly.springapp.configurations;
 
-public class MyUserDetailsService {
-    
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.ltimindtree.iplbidding.entities.User;
+import com.ltimindtree.iplbidding.repositories.UserRepository;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepo;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		User user = userRepo.findByUsername(username);
+		if (user == null)
+			throw new UsernameNotFoundException("user 404");
+		return new UserPrinciple(user);
+	}
+
 }
